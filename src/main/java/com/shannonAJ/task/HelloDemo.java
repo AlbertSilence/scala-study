f686b8707cd9d1a05b5c96dcf664c150cd8d548d
shannonAJ <andy-zhujun-623@163.com>
2017-08-23 23:33:40 +0800

    first commit

---
+++ src/main/java/com/shannonAJ/task/HelloDemo.java
@@ -0,0 +1,14 @@
+package com.shannonAJ.task;
+
+import com.shannonAJ.scala.HelloScala;
+
+/**
+ * Created by ShannonAJ on 2017/8/13.
+ */
+public class HelloDemo {
+    public static void main(String[] args) {
+        System.out.println("use scala class");
+        HelloScala hello = new HelloScala();
+        hello.sayHello("scala");
+    }
+}
---
+++ src/main/scala/com/shannonAJ/scala/HelloScala.scala
@@ -0,0 +1,11 @@
+package com.shannonAJ.scala
+
+/**
+  * Created by ShannonAJ on 2017/8/13.
+  */
+class HelloScala {
+  def sayHello(x:String): Unit = {
+      print("Hello " + x);
+  }
+
+}
